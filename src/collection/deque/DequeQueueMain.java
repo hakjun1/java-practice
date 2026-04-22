5a165c5e871d7eb2ed02259ded4ef5a99fd99ba0
hakjun <hiatus9014@gmail.com>
2026-04-23 00:33:51 +0900

    feat : Deque

---
+++ src/collection/deque/DequeQueueMain.java
@@ -0,0 +1,25 @@
+package collection.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeQueueMain {
+
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();//Queue<Integer>
+//        Deque<Integer> deque = new LinkedList<>();
+
+        //데이터 추가
+        deque.offer(1);
+        deque.offer(2);
+        deque.offer(3);
+        System.out.println(deque);
+
+        System.out.println("deque.peek() = " + deque.peek());
+
+        System.out.println("deque.pop() = " + deque.poll());
+        System.out.println("deque.pop() = " + deque.poll());
+        System.out.println("deque.pop() = " + deque.poll());
+        System.out.println(deque);
+    }
+}
